4b6c4be9aa56f9815333aa84f53da26616785099
Beka <dosxanzada@gmail.com>
2018-11-24 01:44:18 +0600

    finishing---

--- 1rais/js/main.js
+++ 1rais/js/main.js
@@ -1,17 +1,25 @@
-$(document).ready(function () {
-    $("#navbarContent").on("click", "a", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-        event.preventDefault();
 
-        //забираем идентификатор бока с атрибута href
-        var id = $(this).attr('href'),
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-            //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({
-            scrollTop: top
-        }, 1500);
-    });
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
