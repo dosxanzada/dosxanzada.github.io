efebb8fc63e54d732a722826a28106e9ccb993c5
Beka <dosxanzada@gmail.com>
2019-11-11 15:15:01 +0600

    portfolio

---
+++ port/js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+    var typed = new Typed('#typed__text', {
+        strings: ['по доступным ценам.', 'качественно!', 'в кратчайшие сроки', 'индивидуально', ''],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+    });
+});
