690d4b13a9b6b7b232a5ab7d6ba3c313ca65f04e
Beka <dosxanzada@gmail.com>
2019-11-12 16:50:00 +0600

    pft

--- port/js/main.js
+++ port/js/main.js
@@ -1,8 +1,36 @@
 jQuery(document).ready(function () {
+
+    $(".header__menu li a").click(function () {
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top;
+        $("body,html").animate({
+            scrollTop: destination
+        }, 500);
+    });
+
+//    $(window).scroll(function () {
+            //        $('header').toggleClass('scroll');
+            //        $('.header__inner').toggleClass('scroll');
+            //    });
+
     var typed = new Typed('#typed__text', {
         strings: ['по доступным ценам.', 'качественно!', 'в кратчайшие сроки', 'индивидуально', ''],
         typeSpeed: 80,
         backSpeed: 50,
         loop: true
     });
+
+    $('.services__item').hover(function () {
+        $(this).find('span').fadeToggle('fast');
+        $(this).find('.services__body').toggleClass('active');
+    });
+
+    $('.services__info').fadeOut();
+
+    $('.services__item').hover(function () {
+        $(this).find('.services__info').fadeToggle();
+    });
+
+
+
 });
